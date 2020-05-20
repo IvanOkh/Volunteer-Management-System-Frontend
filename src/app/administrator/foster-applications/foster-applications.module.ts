6af099e122e0447f14293b47d4d08ca88d418798
Ivan Okhrimovich <797462@ACDM.DS.SAIT.CA>
2020-05-20 13:10:07 -0600

    To be deleted

--- src/app/administrator/foster-applications/foster-applications.module.ts
+++ src/app/administrator/foster-applications/foster-applications.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FosterArchiveComponent } from './foster-archive/foster-archive.component';
-import { FosterPendingComponent } from './foster-pending/foster-pending.component';
+// import { NgModule } from "@angular/core";
+// import { CommonModule } from "@angular/common";
+// import { FosterArchiveComponent } from "./foster-archive/foster-archive.component";
+// import { FosterPendingComponent } from "./foster-pending/foster-pending.component";
 
-
-
-@NgModule({
-  declarations: [FosterArchiveComponent, FosterPendingComponent],
-  imports: [
-    CommonModule
-  ],
-  exports:[FosterArchiveComponent, FosterPendingComponent]
-})
-export class FosterApplicationsModule { }
+// @NgModule({
+//   declarations: [FosterArchiveComponent, FosterPendingComponent],
+//   imports: [CommonModule],
+//   exports: [FosterArchiveComponent, FosterPendingComponent],
+// })
+// export class FosterApplicationsModule {}
