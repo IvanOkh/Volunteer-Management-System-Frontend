5eb81ec4a360d9879a1e7d297d58b0b5c9973484
Ivan Okhrimovich <797462@ACDM.DS.SAIT.CA>
2020-05-22 13:41:14 -0600

    Improved error handling

--- src/app/shared/services/new-fosters.service.ts
+++ src/app/shared/services/new-fosters.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-import { FosterModel } from '../models/foster.model';
+import { FosterModel } from "../models/foster.model";
 
 /******************************************************************************/
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
-export class FostersService
-{
+export class FostersService {
   private REST_API_SERVER = "http://68.66.193.100:8080/CARS/";
   private CTRL_MAPPING = "fosters/";
 
@@ -22,20 +21,18 @@ export class FostersService
    * Returns the list of fosters in a subscribe-able object.
    * @returns {Observable<FosterModel[]>}
    */
-  public loadAllFosters(): Observable<FosterModel[]>
-  {
-    return this.sendGetAllLoadRequest()
-      .pipe(
-        map((responseData: FosterModel[]) => {
-          const fosterArray: FosterModel[] = [];
-          responseData.forEach((foster: FosterModel) => {
-            if (foster.id || foster.id === 0) {
-              fosterArray.push(foster);
-            }
-          });
-          return fosterArray;
-        })
-      );
+  public loadAllFosters(): Observable<FosterModel[]> {
+    return this.sendGetAllLoadRequest().pipe(
+      map((responseData: FosterModel[]) => {
+        const fosterArray: FosterModel[] = [];
+        responseData.forEach((foster: FosterModel) => {
+          if (foster.id || foster.id === 0) {
+            fosterArray.push(foster);
+          }
+        });
+        return fosterArray;
+      })
+    );
   }
 
   /**
@@ -43,20 +40,23 @@ export class FostersService
    * Returns the list of fosters in a subscribe-able object.
    * @returns {Observable<FosterModel[]>}
    */
-  public loadFosters(activeStatus: boolean): Observable<FosterModel[]>
-  {
-    return this.sendGetAllLoadRequest()
-      .pipe(
-        map((responseData: FosterModel[]) => {
-          const fosterArray: FosterModel[] = [];
+  public loadFosters(activeStatus: boolean): Observable<FosterModel[]> {
+    return this.sendGetAllLoadRequest().pipe(
+      map((responseData: FosterModel[]) => {
+        const fosterArray: FosterModel[] = [];
+        if (responseData != null) {
           responseData.forEach((foster: FosterModel) => {
-            if (foster.id || foster.id === 0 && foster.active === activeStatus) {
+            if (
+              foster.id ||
+              (foster.id === 0 && foster.active === activeStatus)
+            ) {
               fosterArray.push(foster);
             }
           });
-          return fosterArray;
-        })
-      );
+        }
+        return fosterArray;
+      })
+    );
   }
 
   /**
@@ -64,10 +64,8 @@ export class FostersService
    * Returns a subscribe-able object containing the found Foster.
    * @param fosterID
    */
-  public getFoster(fosterID: number): Observable<FosterModel>
-  {
-    return this.sendGetFosterRequest(fosterID)
-    .pipe(
+  public getFoster(fosterID: number): Observable<FosterModel> {
+    return this.sendGetFosterRequest(fosterID).pipe(
       map((responseData: FosterModel[]) => {
         const foster: FosterModel = responseData[0];
         return foster;
@@ -81,31 +79,27 @@ export class FostersService
    * subscibe-able object containing the server success/error response.
    * @param newFoster
    */
-  public addFoster(newFoster: FosterModel): Observable<string>
-  {
-    return this.sendPostRequest(newFoster)
-      .pipe(
-        map((responseData) => {
-          return responseData;
-        })
-      );
+  public addFoster(newFoster: FosterModel): Observable<string> {
+    return this.sendPostRequest(newFoster).pipe(
+      map((responseData) => {
+        return responseData;
+      })
+    );
   }
 
-   /**
+  /**
    * Sends a request to update a foster. The input is a Foster
    * with the same ID as the one we want to update in the backend.
    * Returns a subscibe-able object containing the server success/error
    * response.
    * @param changes
    */
-  public updateFoster(changes: FosterModel): Observable<string>
-  {
-    return this.sendUpdateRequest(changes)
-      .pipe(
-        map((responseData: string) => {
-          return responseData;
-        })
-      );
+  public updateFoster(changes: FosterModel): Observable<string> {
+    return this.sendUpdateRequest(changes).pipe(
+      map((responseData: string) => {
+        return responseData;
+      })
+    );
   }
 
   /**
@@ -114,54 +108,42 @@ export class FostersService
    * able object containing the server success/error response.
    * @param deleteID
    */
-  public removeFoster(deleteID: number): Observable<string>
-  {
-    return this.sendDeleteRequest(deleteID)
-      .pipe(
-        map((responseData) => {
-          return responseData;
-        })
-      );
+  public removeFoster(deleteID: number): Observable<string> {
+    return this.sendDeleteRequest(deleteID).pipe(
+      map((responseData) => {
+        return responseData;
+      })
+    );
   }
 
   /******************************************************************************/
-  private sendGetAllLoadRequest()
-  {
+  private sendGetAllLoadRequest() {
     return this.http.get<FosterModel[]>(
       this.REST_API_SERVER + this.CTRL_MAPPING
     );
   }
 
-  private sendGetFosterRequest(id: number)
-  {
+  private sendGetFosterRequest(id: number) {
     return this.http.get<FosterModel[]>(
       this.REST_API_SERVER + this.CTRL_MAPPING + id
     );
   }
 
-  private sendPostRequest(foster: FosterModel)
-  {
-    return this.http.post(
-      this.REST_API_SERVER + this.CTRL_MAPPING,
-      foster,
-      { responseType: 'text' }
-    );
+  private sendPostRequest(foster: FosterModel) {
+    return this.http.post(this.REST_API_SERVER + this.CTRL_MAPPING, foster, {
+      responseType: "text",
+    });
   }
 
-  private sendUpdateRequest(foster: FosterModel)
-  {
-    return this.http.patch(
-      this.REST_API_SERVER + this.CTRL_MAPPING,
-      foster,
-      { responseType: 'text' }
-    );
+  private sendUpdateRequest(foster: FosterModel) {
+    return this.http.patch(this.REST_API_SERVER + this.CTRL_MAPPING, foster, {
+      responseType: "text",
+    });
   }
 
-  private sendDeleteRequest(id: number)
-  {
-    return this.http.delete(
-      this.REST_API_SERVER + this.CTRL_MAPPING + id,
-      { responseType: 'text' }
-    );
+  private sendDeleteRequest(id: number) {
+    return this.http.delete(this.REST_API_SERVER + this.CTRL_MAPPING + id, {
+      responseType: "text",
+    });
   }
 }
