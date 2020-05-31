e4cb956a2f244c956cd140fdd49c2800fff685a3
Ivan Okhrimovich <797462@ACDM.DS.SAIT.CA>
2020-05-30 20:34:57 -0600

    Account now displays both user types

--- src/app/volunteer/view-volunteer-account/view-volunteer-account.component.ts
+++ src/app/volunteer/view-volunteer-account/view-volunteer-account.component.ts
@@ -9,6 +9,22 @@ import { AuthService } from "src/app/auth/auth.service";
 import { FostersService } from "src/app/shared/services/new-fosters.service";
 import { FosterModel } from "src/app/shared/models/foster.model";
 
+//interface to store either volunteer or foster attributes
+export interface logUser {
+  fname: string;
+  lname: string;
+  address: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  cellPhone: string;
+  homePhone: string;
+  email: string;
+  dateOfBirth?: string;
+  tshirtSize?: string;
+  description?: string;
+}
+
 @Component({
   selector: "app-view-volunteer-account",
   templateUrl: "./view-volunteer-account.component.html",
@@ -17,7 +33,7 @@ import { FosterModel } from "src/app/shared/models/foster.model";
 })
 export class ViewVolunteerAccountComponent implements OnInit {
   @ViewChild("form", { static: false }) form: NgForm;
-  user: VolunteerForm;
+  user: logUser = null;
   isLoading: boolean = false;
 
   constructor(
@@ -31,9 +47,9 @@ export class ViewVolunteerAccountComponent implements OnInit {
     this.fetchUser(Number(session.value.id));
   }
 
-  onSubmit(myForm: NgForm) {
-    this.updateUser();
-  }
+  // onSubmit(myForm: NgForm) {
+  //   this.updateUser();
+  // }
 
   //Adding foster to volunteer - IN PROGRESS
   private fetchUser(id: number) {
@@ -44,31 +60,33 @@ export class ViewVolunteerAccountComponent implements OnInit {
         this.isLoading = false;
       },
       (error: any) => {
-        console.log(error);
-        this.isLoading = false;
+        // console.log(error);
+        // this.isLoading = false;
       }
     );
 
     //working
+
     this.fs.getFoster(id).subscribe(
       (fos: FosterModel) => {
-        // this.user = fos;
+        this.user = fos;
+        this.isLoading = false;
       },
       (error: any) => {
-        console.log(error);
+        // console.log(error);
         this.isLoading = false;
       }
     );
   }
 
-  private updateUser() {
-    this.vs.updateVolunteer(this.user).subscribe(
-      () => {
-        this.fetchUser(this.user.id);
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
-  }
+  // private updateUser() {
+  //   this.vs.updateVolunteer(this.user).subscribe(
+  //     () => {
+  //       this.fetchUser(this.user.id);
+  //     },
+  //     (error: any) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
 }
