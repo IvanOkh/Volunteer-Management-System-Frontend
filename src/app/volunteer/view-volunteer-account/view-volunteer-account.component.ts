700e7e00c969986eb42c1b34a530df1c01d4f212
Ivan Okhrimovich <797462@ACDM.DS.SAIT.CA>
2020-06-03 17:54:11 -0600

    Load/edit account is fully functional

--- src/app/volunteer/view-volunteer-account/view-volunteer-account.component.ts
+++ src/app/volunteer/view-volunteer-account/view-volunteer-account.component.ts
@@ -11,6 +11,7 @@ import { FosterModel } from "src/app/shared/models/foster.model";
 
 //interface to store either volunteer or foster attributes
 export interface logUser {
+  id: number;
   fname: string;
   lname: string;
   address: string;
@@ -20,7 +21,7 @@ export interface logUser {
   cellPhone: string;
   homePhone: string;
   email: string;
-  dateOfBirth?: string;
+
   tshirtSize?: string;
   description?: string;
 }
@@ -35,6 +36,8 @@ export class ViewVolunteerAccountComponent implements OnInit {
   @ViewChild("form", { static: false }) form: NgForm;
   user: logUser = null;
   isLoading: boolean = false;
+  tempfoster: FosterModel = null;
+  tempvolunteer: VolunteerForm = null;
 
   constructor(
     private fs: FostersService,
@@ -47,30 +50,38 @@ export class ViewVolunteerAccountComponent implements OnInit {
     this.fetchUser(Number(session.value.id));
   }
 
-  // onSubmit(myForm: NgForm) {
-  //   this.updateUser();
-  // }
+  onSubmit(myForm: NgForm) {
+    this.updateUser();
+  }
 
   //Adding foster to volunteer - IN PROGRESS
   private fetchUser(id: number) {
     this.isLoading = true;
     this.vs.getVolunteerForm(id).subscribe(
       (vol: VolunteerForm) => {
-        this.user = vol;
-        this.isLoading = false;
+        if (vol != null) {
+          this.tempvolunteer = vol;
+          // console.log(this.tempvolunteer);
+          this.user = vol;
+          // console.log(this.user);
+          this.isLoading = false;
+        }
       },
       (error: any) => {
         // console.log(error);
-        // this.isLoading = false;
+        this.isLoading = false;
       }
     );
 
-    //working
-
     this.fs.getFoster(id).subscribe(
       (fos: FosterModel) => {
-        this.user = fos;
-        this.isLoading = false;
+        if (fos != null) {
+          this.tempfoster = fos;
+          // console.log(this.tempfoster);
+          this.user = fos;
+          // console.log(this.user);
+          this.isLoading = false;
+        }
       },
       (error: any) => {
         // console.log(error);
@@ -79,14 +90,48 @@ export class ViewVolunteerAccountComponent implements OnInit {
     );
   }
 
-  // private updateUser() {
-  //   this.vs.updateVolunteer(this.user).subscribe(
-  //     () => {
-  //       this.fetchUser(this.user.id);
-  //     },
-  //     (error: any) => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
+  private updateUser() {
+    //if user is volunteer
+    if (this.tempvolunteer != null) {
+      this.tempvolunteer.fname = this.user.fname;
+      this.tempvolunteer.lname = this.user.lname;
+      this.tempvolunteer.address = this.user.address;
+      this.tempvolunteer.city = this.user.city;
+      this.tempvolunteer.province = this.user.province;
+      this.tempvolunteer.postalCode = this.user.postalCode;
+      this.tempvolunteer.homePhone = this.user.homePhone;
+      this.tempvolunteer.cellPhone = this.user.cellPhone;
+      this.tempvolunteer.email = this.user.email;
+      this.tempvolunteer.tshirtSize = this.user.tshirtSize;
+      this.tempvolunteer.description = this.user.description;
+      this.vs.updateVolunteer(this.tempvolunteer).subscribe(
+        () => {
+          this.fetchUser(Number(this.user.id));
+        },
+        (error: any) => {
+          // console.log(error);
+        }
+      );
+    }
+    //user is foster
+    else {
+      this.tempfoster.fname = this.user.fname;
+      this.tempfoster.lname = this.user.lname;
+      this.tempfoster.address = this.user.address;
+      this.tempfoster.city = this.user.city;
+      this.tempfoster.province = this.user.province;
+      this.tempfoster.postalCode = this.user.postalCode;
+      this.tempfoster.homePhone = this.user.homePhone;
+      this.tempfoster.cellPhone = this.user.cellPhone;
+      this.tempfoster.email = this.user.email;
+      this.fs.updateFoster(this.tempfoster).subscribe(
+        () => {
+          this.fetchUser(Number(this.user.id));
+        },
+        (error: any) => {
+          // console.log(error);
+        }
+      );
+    }
+  }
 }
