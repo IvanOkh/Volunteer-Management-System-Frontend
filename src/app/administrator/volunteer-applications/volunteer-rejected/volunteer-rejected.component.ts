78c8b0c999530186241a60669d09e4f3927d76f4
Albert Arrieta <albert.arrieta12@gmail.com>
2020-06-20 11:39:50 -0600

    Converted to mat table for Volunteer rejected tab

--- src/app/administrator/volunteer-applications/volunteer-rejected/volunteer-rejected.component.ts
+++ src/app/administrator/volunteer-applications/volunteer-rejected/volunteer-rejected.component.ts
@@ -1,5 +1,13 @@
 // ANGULAR IMPORTS
-import { Component, OnInit, ApplicationInitStatus } from "@angular/core";
+import { Component, OnInit, ApplicationInitStatus, ViewChild, } from "@angular/core";
+
+import {
+  MatPaginator,
+  MatSort,
+  MatTableDataSource,
+  MatDialog,
+} from "@angular/material";
+import { NgForm } from "@angular/forms";
 
 // CUSTOM COMPONENTS
 import { VolunteerForm } from "src/app/shared/models/volunteer-form.model";
@@ -15,8 +23,20 @@ import { VolunteerService } from "src/app/shared/services/new-volunteer.service"
 export class VolunteerRejectedComponent implements OnInit {
   appList: VolunteerApplication[] = [];
   isLoading: boolean = false;
+  application: VolunteerApplication;
+  //Mat table configuration
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild("form", { static: false }) form: NgForm;
 
-  constructor(private test: VolunteerService) {}
+  displayedColumns: string[] = [
+    "submissionDate",
+    "applicant",
+    "email",
+    "rejectionNote",
+  ];
+
+  constructor(public dialog: MatDialog, private vs: VolunteerService) {}
 
   ngOnInit() {
     // this.loadActiveApplicants();  // this one loads all currently active applicants
@@ -32,13 +52,85 @@ export class VolunteerRejectedComponent implements OnInit {
 
   private loadRejectedApplicants(): void {
     this.isLoading = true;
-    this.test.loadRejectedApplicants().subscribe(
+    this.vs.loadRejectedApplicants().subscribe(
       (applicants: VolunteerApplication[]) => {
         this.appList = [];
         applicants.forEach((applicant: VolunteerApplication) => {
           this.appList.push(applicant);
         });
         this.isLoading = false;
+      },
+      (error: any) => {
+        console.log(error);
+        this.isLoading = false;
+      }
+    );
+  }
+
+    /**
+   * Changes the 'rejected' attribute of the volunteer application from 'false' to 'true'.
+   * @param fosterID
+   */
+  private sendToPending(volunteerID: number): void {
+    // guard condition if the volunteerID returend by the DOM is undefiend
+    if (!volunteerID) {
+      console.log("ERROR: volunteer id is" + volunteerID);
+      return;
+    }
+
+    this.vs.getVolunteerApplication(volunteerID).subscribe(
+      (volunteer: VolunteerApplication) => {
+        if (volunteer) {
+          volunteer.rejected = false;
+          this.updateApplication(volunteer);
+        } else {
+          console.log("Volunteer not found!");
+        }
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+ /**
+   * Takes the passed volunteer application model and requests an update to the backend with the attributes provided.
+   * @param changes
+   */
+  private updateApplication(changes: VolunteerApplication): void {
+    this.vs.updateVolunteerApplication(changes).subscribe(
+      (status: any) => {
+        this.loadRejectedApplicants();
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+/**
+   * Permanently deletes the volunteer application with the passed id. 
+   * @param id
+   */
+  private deleteApplication(id: number): void {
+    this.vs.removeApplication(id).subscribe(
+      (status: any) => {
+        this.loadRejectedApplicants();
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private getRecord(id: number) {
+    this.isLoading = true;
+    this.appList.forEach(
+      (element) => {
+        if (element.id === id) {
+          this.application = element;
+          this.isLoading = false;
+        }
       },
       (error: any) => {
         console.log(error);
