f241339d36bb969a1850edab217b89ead81ec126
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-08 19:02:48 -0600

    Adjusted for lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from "@angular/http";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatTableModule } from "@angular/material";
-import {MatProgressSpinnerModule} from '@angular/material'
+import { MatProgressSpinnerModule } from "@angular/material";
 
 // COMPONENT IMPORTS
 import { AppComponent } from "./app.component";
@@ -20,26 +20,22 @@ import { ResetPasswordComponent } from "./shared/components/reset-password/reset
 import { MainPageComponent } from "./shared/components/main-page/main-page.component";
 
 import { FosterFormComponent } from "./forms/foster-form/foster-form.component";
-import { FamilyMembersComponent } from "./forms/foster-form/family-members/family-members.component";
-import { DogFormComponent } from "./forms/dog-form/dog-form.component";
 import { VolunteerFormComponent } from "./forms/volunteer-form/volunteer-form.component";
-import { CatFormModule } from "./forms/cat-form/cat-form.module";
-
+import { FamilyMembersComponent } from "./forms/foster-form/family-members/family-members.component";
+// import { DogFormComponent } from "./forms/dog-form/dog-form.component";
+// import { CatFormModule } from "./forms/cat-form/cat-form.module";
 import { VolunteerModule } from "./volunteer/volunteer.module";
 import { AdministratorModule } from "./administrator/administrator.module";
-import { NaPipe } from './shared/pipes/na.pipe';
-
+// import { NaPipe } from "./shared/pipes/na.pipe";
+// import { Router } from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-
     FosterFormComponent,
-    DogFormComponent,
     VolunteerFormComponent,
     FamilyMembersComponent,
-
     LoadingSpinnerComponent,
     FooterComponent,
     ResetPasswordComponent,
@@ -56,12 +52,12 @@ import { NaPipe } from './shared/pipes/na.pipe';
     MatProgressSpinnerModule,
     // CUSTOM MODULES
     AppRoutingModule,
-    CatFormModule,
+    // CatFormModule,
     VolunteerModule,
     AdministratorModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
