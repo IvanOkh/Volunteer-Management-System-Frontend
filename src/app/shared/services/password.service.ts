46cbc1a9b2c2d5a59655521c15ef772511af94ef
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-16 19:22:19 -0600

    New password recovery service

--- src/app/shared/services/password.service.ts
+++ src/app/shared/services/password.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { NewPassword } from "../models/new-password.model";
 
 @Injectable({
   providedIn: "root",
@@ -9,9 +10,29 @@ import { map } from "rxjs/operators";
 export class passwordService {
   private REST_API_SERVER = "http://68.66.193.100:8080/CARS/"; // REST API endpoint
   private CTRL_MAPPING = "reset";
+  private UUID_MAPPING = "password";
+
+  holder: NewPassword = null;
 
   constructor(private http: HttpClient) {}
 
+  //sequre request with uuid
+  public sendNewPassword(uuid: string, pass: string): Observable<string> {
+    this.holder = new NewPassword(uuid, pass);
+    return this.sendPostUUID(this.holder).pipe(
+      map((responseData) => {
+        return responseData;
+      })
+    );
+  }
+
+  private sendPostUUID(new_pass: NewPassword) {
+    return this.http.post(this.REST_API_SERVER + this.UUID_MAPPING, new_pass, {
+      responseType: "text",
+    });
+  }
+
+  //initial recovery request
   public sendRecovery(email: string): Observable<string> {
     return this.sendPostRequest(email).pipe(
       map((responseData) => {
