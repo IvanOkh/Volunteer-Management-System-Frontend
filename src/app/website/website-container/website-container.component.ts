70324bdf27807e62b49d427972ec26d125587a1e
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-27 19:04:12 -0600

    New container for the site development

---
+++ src/app/website/website-container/website-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-website-container',
+  templateUrl: './website-container.component.html',
+  styleUrls: ['./website-container.component.css']
+})
+export class WebsiteContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
