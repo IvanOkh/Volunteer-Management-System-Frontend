355656e2fbb399b628e6390fcef88c0c91d13273
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-27 19:15:19 -0600

    Cleanup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 // ANGULAR IMPORTS
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 // CUSTOM COMPONENTS
@@ -13,6 +13,7 @@ import { Observable } from "rxjs";
   styleUrls: ["./auth.component.css"],
 })
 export class AuthComponent {
+  // @ViewChild("authForm", { static: false }) sgnForm: NgForm;
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -22,48 +23,53 @@ export class AuthComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit(form: NgForm) {
-    var serchfind: boolean;
+    // this.sgnForm = form.value.email;
+    // this.isLoading = true;
+    // var serchfind: boolean;
     //return if form is invalid
     if (!form.valid) {
       this.errorMessage = true;
-    }
-    //get email and pass from the submited form
-    const email = form.value.email;
-    const password = form.value.password;
-    //observable for the login response data
-    let authObservable: Observable<AuthResponseData>;
-
-    //EMAIL REGEX
-    let regexp = new RegExp(
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-    //test user input
-    serchfind = regexp.test(email);
-
-    if (serchfind === true) {
-      this.isLoading = true;
-      authObservable = this.authService.login(email, password);
-
-      authObservable.subscribe(
-        (responseData) => {
-          //console.log(responseData);
-          this.isLoading = false;
-          if (responseData.role === "admin") {
-            this.router.navigate(["/admin"]);
-          } else {
-            this.router.navigate(["/volunteer"]);
-          }
-        },
-        (errorMessage) => {
-          //console.log(errorMessage);
-          // this.error = errorMessage;
-          this.errorMessage = true;
-          this.isLoading = false;
-        }
-      );
+      // this.isLoading = false;
+      // this.sgnForm.reset();
+      // console.log("invalid input");
       // form.reset();
     } else {
-      this.errorMessage = true;
+      //get email and pass from the submited form
+      const email = form.value.email;
+      const password = form.value.password;
+      //observable for the login response data
+      let authObservable: Observable<AuthResponseData>;
+
+      //EMAIL REGEX
+      let regexp = new RegExp(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+      //test user input
+      let serchfind = regexp.test(email);
+
+      if (serchfind === true) {
+        this.isLoading = true;
+        authObservable = this.authService.login(email, password);
+
+        authObservable.subscribe((responseData) => {
+          // console.log(responseData);
+
+          this.isLoading = false;
+
+          if (responseData.role === null && responseData.token === null) {
+            this.errorMessage = true;
+            this.isLoading = false;
+          } else if (responseData.role === "admin") {
+            this.router.navigate(["/admin"]);
+          } else if (responseData.role === "regular") {
+            this.router.navigate(["/volunteer"]);
+          }
+        });
+        // form.reset();
+      } else {
+        this.errorMessage = true;
+        this.isLoading = false;
+      }
     }
   }
 }
