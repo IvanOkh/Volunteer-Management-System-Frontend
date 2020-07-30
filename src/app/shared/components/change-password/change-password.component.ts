202f965cfd403ba3f5e3b8d7a281c7ecfa8b952b
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-29 19:43:00 -0600

    Developing logic for recovery

--- src/app/shared/components/change-password/change-password.component.ts
+++ src/app/shared/components/change-password/change-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { ActivatedRoute, Router, ParamMap, Params } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { passwordService } from "../../services/password.service";
 
 @Component({
   selector: "app-change-password",
@@ -7,12 +9,57 @@ import { ActivatedRoute, Router, ParamMap } from "@angular/router";
   styleUrls: ["./change-password.component.css"],
 })
 export class ChangePasswordComponent implements OnInit {
-  hhh: string = "empty";
+  UUID: string = "";
+  newPassword: string = "";
+  message: string = "";
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private passwordService: passwordService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    console.log(this.route.snapshot.params.id);
-    this.hhh = this.route.snapshot.params.id;
+    let sub = this.route.queryParams.subscribe((queryParams: Params) => {
+      this.UUID = queryParams["uuid"];
+      console.log(this.UUID);
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value.newPassword);
+    console.log(form.value.confirmedNewPassword);
+
+    if (form.value.newPassword === form.value.confirmedNewPassword) {
+      this.recover(this.UUID, form.value.newPassword);
+    } else {
+      console.log("Reseted invalid form");
+      form.resetForm();
+    }
+  }
+
+  private recover(uuid: string, password: string): void {
+    this.passwordService.sendNewPassword(uuid, password).subscribe(
+      (response) => {
+        console.log(response);
+        if (response === "Request processed.") {
+          this.message =
+            "Your request has been processed. If the email matches our records, you will receive an email. Please check the Spam folder if the email response was not received within 12 hours.";
+          // this.form.reset();
+          // this.isLoading = false;
+        } else {
+          this.message =
+            "We could not process your request. Make sure you are entering correct email address";
+          // this.isLoading = false;
+        }
+      },
+      (error: any) => {
+        this.message =
+          "Unfortunately, we encountered an error. Please check your internet connection or try again later";
+        // this.form.reset();
+        // this.isLoading = false;
+      }
+    );
+    // this.submitted = true;
   }
 }
