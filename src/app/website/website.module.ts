ecada02512189a5c9645bb9807a3bc4eb2358187
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-05 17:04:31 -0600

    Declared new components + export PNF

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -7,17 +7,27 @@ import { HttpModule } from "@angular/http";
 import { HttpClientModule } from "@angular/common/http";
 import { WebsiteContainerComponent } from "./website-container/website-container.component";
 import { WebsiteRoutingModule } from "./website-routing.module";
+import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
+import { VolunteerModule } from "../volunteer/volunteer.module";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 // MODULE DEFINITION
 @NgModule({
-  declarations: [WebsiteContainerComponent],
+  declarations: [
+    WebsiteContainerComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    HomePageComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
     HttpModule,
     HttpClientModule,
     WebsiteRoutingModule,
+    VolunteerModule,
   ],
-  exports: [],
+  exports: [PageNotFoundComponent],
   providers: [],
   bootstrap: [],
 })
