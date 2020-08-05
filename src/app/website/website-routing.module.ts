6dd8e4cb3b3e092f33c9d71c08994c172fbf4e24
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-05 17:04:01 -0600

    Updated routes

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -1,17 +1,20 @@
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { WebsiteContainerComponent } from "./website-container/website-container.component";
+import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 const websiteRoutes: Routes = [
   {
     path: "",
     component: WebsiteContainerComponent,
-    //feel free to add child routes below
     children: [
-      //   { path: "forstroute", component: CompontnName },
-      //   { path: "secondroute", component: CompontnName },
-      { path: "", redirectTo: "develop", pathMatch: "full" },
-      { path: "**", component: WebsiteContainerComponent },
+      // { path: "", redirectTo: "develop", pathMatch: "full" },
+      { path: "home", component: HomePageComponent },
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      //feel free to add child routes below this line
+
+      { path: "**", component: PageNotFoundComponent },
     ],
   },
 ];
