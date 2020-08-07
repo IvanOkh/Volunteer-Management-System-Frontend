5b361aae854948a0a98a9025cc03f3557dc240c5
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-06 20:26:01 -0600

    + goptek page

---
+++ src/app/website/goptek-page/goptek-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goptek-page',
+  templateUrl: './goptek-page.component.html',
+  styleUrls: ['./goptek-page.component.css']
+})
+export class GoptekPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
