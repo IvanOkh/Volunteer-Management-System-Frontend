b010b29357c97294f5a3962af8688f1456b75ee1
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-07 13:40:04 -0600

    Router links update

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,8 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { WebsiteContainerComponent } from "./website-container/website-container.component";
 import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
-import { HomePageComponent } from "./home-page/home-page.component";
+import { HomePageComponent } from "./_pages/home-page/home-page.component";
+import { OurAnimalsPageComponent } from "./_pages/our-animals-page/our-animals-page.component";
 
 const websiteRoutes: Routes = [
   {
@@ -11,6 +12,8 @@ const websiteRoutes: Routes = [
     children: [
       // { path: "", redirectTo: "develop", pathMatch: "full" },
       { path: "home", component: HomePageComponent },
+      { path: "our-animals", component: OurAnimalsPageComponent },
+
       { path: "", redirectTo: "home", pathMatch: "full" },
       //feel free to add child routes below this line
 
