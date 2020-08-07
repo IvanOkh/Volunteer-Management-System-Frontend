147ffafb99b733e103d7bc98bd6d35b7dc312ee5
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-06 20:26:45 -0600

    Added 12 page components

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -11,6 +11,18 @@ import { PageNotFoundComponent } from "../shared/components/page-not-found/page-
 import { VolunteerModule } from "../volunteer/volunteer.module";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { AdoptionPageComponent } from "./adoption-page/adoption-page.component";
+import { OurAnimalsPageComponent } from "./our-animals-page/our-animals-page.component";
+import { VolunteerPageComponent } from "./volunteer-page/volunteer-page.component";
+import { FosterPageComponent } from './foster-page/foster-page.component';
+import { DonateFundsPageComponent } from './donate-funds-page/donate-funds-page.component';
+import { DonateSuppliesPageComponent } from './donate-supplies-page/donate-supplies-page.component';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { UpcomingEventsPageComponent } from './upcoming-events-page/upcoming-events-page.component';
+import { OurPartnersPageComponent } from './our-partners-page/our-partners-page.component';
+import { HappyTailsPageComponent } from './happy-tails-page/happy-tails-page.component';
+import { GoptekPageComponent } from './goptek-page/goptek-page.component';
 // MODULE DEFINITION
 @NgModule({
   declarations: [
@@ -18,6 +30,18 @@ import { HomePageComponent } from "./home-page/home-page.component";
     PageNotFoundComponent,
     NavbarComponent,
     HomePageComponent,
+    AdoptionPageComponent,
+    OurAnimalsPageComponent,
+    VolunteerPageComponent,
+    FosterPageComponent,
+    DonateFundsPageComponent,
+    DonateSuppliesPageComponent,
+    ContactUsPageComponent,
+    FaqPageComponent,
+    UpcomingEventsPageComponent,
+    OurPartnersPageComponent,
+    HappyTailsPageComponent,
+    GoptekPageComponent,
   ],
   imports: [
     CommonModule,
