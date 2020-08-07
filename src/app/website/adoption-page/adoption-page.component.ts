6c8c942dee692694b0e76e64fb1c6aca525ae61d
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-06 20:22:33 -0600

    + adoption page

---
+++ src/app/website/adoption-page/adoption-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adoption-page',
+  templateUrl: './adoption-page.component.html',
+  styleUrls: ['./adoption-page.component.css']
+})
+export class AdoptionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
