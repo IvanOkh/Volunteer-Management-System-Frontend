cde1850ea9d7961b41469a5647d37216c310a1b6
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-06 20:24:20 -0600

    + upcoming events page

---
+++ src/app/website/upcoming-events-page/upcoming-events-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-events-page',
+  templateUrl: './upcoming-events-page.component.html',
+  styleUrls: ['./upcoming-events-page.component.css']
+})
+export class UpcomingEventsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
