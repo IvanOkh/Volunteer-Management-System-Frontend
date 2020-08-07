b7f48a6f0ee4d19db0a58dfbf9ce470e0bb920d3
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-06 20:23:33 -0600

    + donate supplies page

---
+++ src/app/website/donate-supplies-page/donate-supplies-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donate-supplies-page',
+  templateUrl: './donate-supplies-page.component.html',
+  styleUrls: ['./donate-supplies-page.component.css']
+})
+export class DonateSuppliesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
