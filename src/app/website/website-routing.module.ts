de0d2c0d8c7f64cf08f54ddb4f0e98e63ecc0c02
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-07 14:13:56 -0600

    Added remaining routes and router links

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -4,6 +4,16 @@ import { WebsiteContainerComponent } from "./website-container/website-container
 import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
 import { HomePageComponent } from "./_pages/home-page/home-page.component";
 import { OurAnimalsPageComponent } from "./_pages/our-animals-page/our-animals-page.component";
+import { VolunteerPageComponent } from "./_pages/volunteer-page/volunteer-page.component";
+import { FosterPageComponent } from "./_pages/foster-page/foster-page.component";
+import { ContactUsPageComponent } from "./_pages/contact-us-page/contact-us-page.component";
+import { FaqPageComponent } from "./_pages/faq-page/faq-page.component";
+import { UpcomingEventsPageComponent } from "./_pages/upcoming-events-page/upcoming-events-page.component";
+import { OurPartnersPageComponent } from "./_pages/our-partners-page/our-partners-page.component";
+import { HappyTailsPageComponent } from "./_pages/happy-tails-page/happy-tails-page.component";
+import { GoptekPageComponent } from "./_pages/goptek-page/goptek-page.component";
+import { DonateFundsPageComponent } from "./_pages/donate-funds-page/donate-funds-page.component";
+import { DonateSuppliesPageComponent } from "./_pages/donate-supplies-page/donate-supplies-page.component";
 
 const websiteRoutes: Routes = [
   {
@@ -13,7 +23,16 @@ const websiteRoutes: Routes = [
       // { path: "", redirectTo: "develop", pathMatch: "full" },
       { path: "home", component: HomePageComponent },
       { path: "our-animals", component: OurAnimalsPageComponent },
-
+      { path: "volunteer", component: VolunteerPageComponent },
+      { path: "foster", component: FosterPageComponent },
+      { path: "donate-funds", component: DonateFundsPageComponent },
+      { path: "donate-supplies", component: DonateSuppliesPageComponent },
+      { path: "contact-us", component: ContactUsPageComponent },
+      { path: "faq", component: FaqPageComponent },
+      { path: "events", component: UpcomingEventsPageComponent },
+      { path: "partners", component: OurPartnersPageComponent },
+      { path: "happy-tails", component: HappyTailsPageComponent },
+      { path: "goptek", component: GoptekPageComponent },
       { path: "", redirectTo: "home", pathMatch: "full" },
       //feel free to add child routes below this line
 
