908a6b09fd2c54896fa950e1a53e8548aa9d78bd
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-07 13:39:47 -0600

    Updated imports

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -8,21 +8,22 @@ import { HttpClientModule } from "@angular/common/http";
 import { WebsiteContainerComponent } from "./website-container/website-container.component";
 import { WebsiteRoutingModule } from "./website-routing.module";
 import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
+
+import { NavbarComponent } from "./_reusables/navbar/navbar.component";
+import { HomePageComponent } from "./_pages/home-page/home-page.component";
+import { AdoptionPageComponent } from "./_pages/adoption-page/adoption-page.component";
+import { OurAnimalsPageComponent } from "./_pages/our-animals-page/our-animals-page.component";
+import { VolunteerPageComponent } from "./_pages/volunteer-page/volunteer-page.component";
+import { FosterPageComponent } from "./_pages/foster-page/foster-page.component";
+import { DonateFundsPageComponent } from "./_pages/donate-funds-page/donate-funds-page.component";
+import { DonateSuppliesPageComponent } from "./_pages/donate-supplies-page/donate-supplies-page.component";
+import { ContactUsPageComponent } from "./_pages/contact-us-page/contact-us-page.component";
+import { FaqPageComponent } from "./_pages/faq-page/faq-page.component";
+import { UpcomingEventsPageComponent } from "./_pages/upcoming-events-page/upcoming-events-page.component";
+import { OurPartnersPageComponent } from "./_pages/our-partners-page/our-partners-page.component";
+import { HappyTailsPageComponent } from "./_pages/happy-tails-page/happy-tails-page.component";
+import { GoptekPageComponent } from "./_pages/goptek-page/goptek-page.component";
 import { VolunteerModule } from "../volunteer/volunteer.module";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { AdoptionPageComponent } from "./adoption-page/adoption-page.component";
-import { OurAnimalsPageComponent } from "./our-animals-page/our-animals-page.component";
-import { VolunteerPageComponent } from "./volunteer-page/volunteer-page.component";
-import { FosterPageComponent } from './foster-page/foster-page.component';
-import { DonateFundsPageComponent } from './donate-funds-page/donate-funds-page.component';
-import { DonateSuppliesPageComponent } from './donate-supplies-page/donate-supplies-page.component';
-import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
-import { FaqPageComponent } from './faq-page/faq-page.component';
-import { UpcomingEventsPageComponent } from './upcoming-events-page/upcoming-events-page.component';
-import { OurPartnersPageComponent } from './our-partners-page/our-partners-page.component';
-import { HappyTailsPageComponent } from './happy-tails-page/happy-tails-page.component';
-import { GoptekPageComponent } from './goptek-page/goptek-page.component';
 // MODULE DEFINITION
 @NgModule({
   declarations: [
