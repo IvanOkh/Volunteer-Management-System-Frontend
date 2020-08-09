1e046223eb1a7c4568561579ffd0c112a0870bc7
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-09 17:17:02 -0600

    Complete error cases and user handling

--- src/app/shared/components/change-password/change-password.component.ts
+++ src/app/shared/components/change-password/change-password.component.ts
@@ -9,9 +9,10 @@ import { passwordService } from "../../services/password.service";
   styleUrls: ["./change-password.component.css"],
 })
 export class ChangePasswordComponent implements OnInit {
-  UUID: string = "";
-  newPassword: string = "";
+  private UUID: string = "";
   message: string = "";
+  isLoading: boolean = false;
+  processed: boolean = false;
 
   constructor(
     private passwordService: passwordService,
@@ -22,48 +23,59 @@ export class ChangePasswordComponent implements OnInit {
   ngOnInit() {
     let sub = this.route.queryParams.subscribe((queryParams: Params) => {
       this.UUID = queryParams["uuid"];
-      console.log(this.UUID);
+      // console.log(this.UUID);
     });
+    this.onScroll();
   }
 
   onSubmit(form: NgForm) {
     // console.log(form.value.newPassword);
     // console.log(form.value.confirmedNewPassword);
+    // console.log(form.value.newPassword.length);
+    if (
+      form.value.newPassword.length === 0 &&
+      form.value.confirmedNewPassword.length === 0
+    ) {
+      this.message =
+        "Your new password can't be empty. Try to use at least 8 characters";
+      return;
+    }
+    if (
+      form.value.newPassword.length < 8 &&
+      form.value.confirmedNewPassword.length < 8
+    ) {
+      this.message =
+        "Your new password is too short. Try to use at least 8 characters";
+      return;
+    }
     if (form.value.newPassword === form.value.confirmedNewPassword) {
       this.recover(this.UUID, form.value.newPassword);
     } else {
       this.message = "Please make sure both passwords match each other";
-      // form.resetForm();
     }
   }
 
   private recover(uuid: string, password: string): void {
+    this.isLoading = true;
     this.passwordService.sendNewPassword(uuid, password).subscribe(
       (response) => {
-        console.log(response);
+        // console.log(response);
         if (response) {
-          console.log("i am in processed");
+          // console.log("i am in processed");
           this.message =
-            "Your request has been processed. Please try to login using new password.";
+            "Your request has been processed. Please try to login using new password";
           // this.form.reset();
-          // this.isLoading = false;
+          this.processed = true;
+          this.isLoading = false;
         }
-        // else {
-        //   console.log("i am in else");
-        //   this.message =
-        //     "We could not process your request. Make sure you are entering correct email address";
-        //   // this.isLoading = false;
-        // }
       },
       (error: any) => {
-        console.log("received an error");
+        // console.log("received an error");
         this.message =
           "Unfortunately, we encountered an error. Please check your internet connection or try again later";
-        // this.form.reset();
-        // this.isLoading = false;
+        this.isLoading = false;
       }
     );
-    // this.submitted = true;
   }
 
   onScroll() {
