5b3cb8ad559c82f779e8df279f4d38ddd691ed34
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-08-16 16:39:38 -0600

    + template init based on observable

--- src/app/volunteer/volunteer-side-events/volunteer-side-events.component.ts
+++ src/app/volunteer/volunteer-side-events/volunteer-side-events.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { VolunteerEventsService } from "src/app/shared/services/volunteer-events.service";
 import { EventModel } from "src/app/shared/models/event.model";
 import { EventStaffModel } from "src/app/shared/models/event-staff.model";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-volunteer-side-events",
@@ -10,37 +11,41 @@ import { EventStaffModel } from "src/app/shared/models/event-staff.model";
 })
 export class VolunteerSideEventsComponent implements OnInit {
   numberArray = []; //array to iterate "less/more details"
-  activeEvents: EventModel[] = []; //array of active user specific events
+  activeEvents$: Observable<EventModel[]>; //array of active user specific events
   eventArrayHolder: EventStaffModel[] = []; //array of user specific event subscriptions
-  isLoading: boolean = false; // loading status
-  isLoading2: boolean = false;
+  // isLoading: boolean = false; // loading status
+  // isLoading2: boolean = false;
 
   constructor(private VES: VolunteerEventsService) {}
 
   ngOnInit() {
     document.body.scrollTop = 0;
+    this.VES.initializeData();
+    this.activeEvents$ = this.VES.backEndEvents$;
     this.initData();
   }
 
   private initData() {
-    this.isLoading = true;
-    this.isLoading2 = true;
+    // this.isLoading = true;
+    // this.isLoading2 = true;
     //start up service
-    this.VES.initializeData();
+
     //subscribe to two observables to keep updated events and event-registration arrays
     this.VES.eventStaffData.subscribe((data) => {
       if (data != null) {
         this.eventArrayHolder = data;
-        this.isLoading = false;
+        // this.isLoading = false;
       }
     });
+
+    // this.activeEvents = this.VES.backEndEvents$;
     //subscribe to keep up to date events
-    this.VES.backEndEvents.subscribe((idata) => {
-      if (idata != null) {
-        this.activeEvents = idata;
-        this.isLoading2 = false;
-      }
-    });
+    // this.VES.backEndEvents$.subscribe((idata) => {
+    //   if (idata != null) {
+    //     this.activeEvents = idata;
+    //     this.isLoading2 = false;
+    //   }
+    // });
   }
 
   //Method that checks if user is registered for event in a currently displayed event card.
