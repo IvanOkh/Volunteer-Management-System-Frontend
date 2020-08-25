9b936bdbf27184d64256d1381b7ce0bcbfeecf6d
Albert Arrieta <albert.arrieta12@gmail.com>
2020-08-25 13:31:18 -0600

    Fixed Foster Section styling

--- src/app/website/_reusables/foster-home-section/foster-home-section.component.ts
+++ src/app/website/_reusables/foster-home-section/foster-home-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-foster-home-section',
   templateUrl: './foster-home-section.component.html',
-  styleUrls: ['./foster-home-section.component.css']
+  styleUrls: ['./foster-home-section.component.css', '../../website-style.css']
 })
 export class FosterHomeSectionComponent implements OnInit {
 
