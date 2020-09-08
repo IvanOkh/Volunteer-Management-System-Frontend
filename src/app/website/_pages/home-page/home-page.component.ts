de0ff024ae67e6564767f52c834570b5d9577bae
Albert Arrieta <albert.arrieta12@gmail.com>
2020-09-08 12:24:15 -0600

    +Featured events cards onto homepage

--- src/app/website/_pages/home-page/home-page.component.ts
+++ src/app/website/_pages/home-page/home-page.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Observable } from "rxjs";
+import { EventModel } from "src/app/shared/models/event.model";
+import { EventsService } from "src/app/shared/services/events.service";
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.css", "../../website-style.css"],
 })
 export class HomePageComponent implements OnInit {
-  constructor() {}
+
+  webSiteEvents$: Observable<EventModel[]>;
+
+  constructor(private eventService: EventsService) {}
 
   ngOnInit() {
     document.body.scrollTop = 0;
+    this.webSiteEvents$ = this.eventService.loadPublicEvents();
   }
 }
