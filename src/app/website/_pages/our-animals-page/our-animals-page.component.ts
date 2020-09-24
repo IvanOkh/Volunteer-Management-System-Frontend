f7727fbf48484e2d6313b984ff404d7a63e46a1d
Albert Arrieta <albert.arrieta12@gmail.com>
2020-09-24 14:39:28 -0600

    Tab selection functionality in 'our animals'

--- src/app/website/_pages/our-animals-page/our-animals-page.component.ts
+++ src/app/website/_pages/our-animals-page/our-animals-page.component.ts
@@ -6,9 +6,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./our-animals-page.component.css", "../../website-style.css"],
 })
 export class OurAnimalsPageComponent implements OnInit {
+
+  openTab: string;
+
   constructor() {}
 
   ngOnInit() {
     document.body.scrollTop = 0;
+    this.openTab = 'ac';
   }
+
+
+  /**
+   * Determines which tab is selected in the component. Default is "ac".
+   * 
+   * ac = Adult Cat
+   * ki = Kittens
+   * ad = Adult Dogs
+   * pu = Puppies
+   */
+  tabChange(tab: string) {
+    this.openTab = tab;  
+  }
+
 }
