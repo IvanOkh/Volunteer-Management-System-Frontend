25f42eb8a279e0f155db922d7c89eddbda8a63fc
Ivan Okhrimovich <iokhrimovich@atrematech.com>
2020-11-14 17:23:29 -0700

    Added pointer and link to dev page

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-footer",
@@ -6,7 +7,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  onScroll() {
+    document.body.scrollTop = 0;
+  }
+
+  onOkClick() {
+    this.onScroll();
+    this.router.navigate(["/public/goptek"]);
+  }
 }
