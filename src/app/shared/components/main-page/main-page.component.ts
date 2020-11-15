1b9c353ca8509f0d7115465cb694b422dac94018
Ivan Okhrimovich <iokhrimovich@atrematech.com>
2020-11-14 17:25:16 -0700

    Added navlink to the home page

--- src/app/shared/components/main-page/main-page.component.ts
+++ src/app/shared/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-main-page",
@@ -6,7 +7,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./main-page.component.css"],
 })
 export class MainPageComponent implements OnInit {
-  constructor() {}
+  
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  onScroll() {
+    document.body.scrollTop = 0;
+  }
+
+  onOkClick() {
+    this.onScroll();
+    this.router.navigate(["/public/home"]);
+  }
 }
